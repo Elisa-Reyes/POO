32f056fc32382462a2b98756593d4dc9f197e1c8
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2025-12-01 23:58:44 -0600

    commit_01_12_25

---
+++ POO/P3/coches_system/main.py
@@ -0,0 +1,27 @@
+'''
+1er Diciembre
+    1) Implememntar mvc
+    2) POO
+    3) Interfaces:
+        3.1 menu_principal()
+        3.2 menu_acciones()
+        3.3 insertar_autos()
+        3.4 consultar_autos()
+        3.5 cambiar_autos()
+        3.6 borrar_autos()
+    Entregables:
+    **Estructura principal del proyecto en MVC
+    **Módulo principal Main
+    **Nombre del commit: commit_01_12_25
+'''
+
+from tkinter import *
+
+class App:
+    def __init__(self):
+        pass
+
+if __name__=="__main__":
+    ventana=Tk()
+    app=App(ventana)
+    ventana.mainloop()
